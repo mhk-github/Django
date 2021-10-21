b68eb34c57b1c8f405225f7c4f03fa90b879323f
Mohammad Haroon <72883489+mhk-github@users.noreply.github.com>
2021-10-21 15:15:37 -0300

    Model tests

--- todos_project/todos/tests.py
+++ todos_project/todos/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from .models import Todo
+
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(task='Test task 1', location='London')
+        Todo.objects.create(task='Test task 2', location='Melbourne')
+
+    def test_todo_task_found_correctly(self):
+        task_1 = Todo.objects.get(task='Test task 1')
+        task_2 = Todo.objects.get(task='Test task 2')
+        self.assertEqual(task_1.location, 'London')
+        self.assertEqual(task_2.location, 'Melbourne')
+
+    def test_todo_task_has_temperature(self):
+        task_1 = Todo.objects.get(task='Test task 1')
+        task_2 = Todo.objects.get(task='Test task 2')
+        self.assertIsNotNone(task_1.temperature)
+        self.assertIsNotNone(task_2.temperature)
+
+    def test_todo_task_starts_undone(self):
+        task_1 = Todo.objects.get(task='Test task 1')
+        task_2 = Todo.objects.get(task='Test task 2')
+        self.assertFalse(task_1.done)
+        self.assertFalse(task_2.done)
+
+    def test_todo_task_gives_valid_string(self):
+        task_1 = Todo.objects.get(task='Test task 1')
+        task_2 = Todo.objects.get(task='Test task 2')
+        string_1 = f"{task_1}"
+        string_2 = f"{task_2}"
+        self.assertIn('Test task 1', string_1)
+        self.assertIn('Test task 2', string_2)
