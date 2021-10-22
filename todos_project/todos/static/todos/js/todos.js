bdc2c2aea9e1aeb1e38a9b3a81e4e54786aaf862
Mohammad Haroon <72883489+mhk-github@users.noreply.github.com>
2021-10-22 02:56:13 -0300

    Async version

--- todos_project/todos/static/todos/js/todos.js
+++ todos_project/todos/static/todos/js/todos.js
@@ -19,7 +19,8 @@ var HOT_COLOUR = 'red';
 var WARM_COLOUR = 'gold';
 var COLD_COLOUR = 'blue';
 
-var REGEX = new RegExp('<li>Location\s*:\s*(.*)\s*</li>', 'm');
+var REGEX_TASK = new RegExp('<li>Location\s*:\s*(.*)\s*</li>', 'm');
+var REGEX_LOCATION = new RegExp('id="(.*)"', 'm');
 var BASE_URL = 'http://localhost:8000/todos/weather/?q=';
 
 
@@ -48,40 +49,61 @@ $(document).ready(function(){
     for (var i = 0; i < $tasks.length; i++) {
         var $current_task = $tasks.eq(i);
         var $task_html = $current_task.html();
-        var $task_location = $task_html.match(REGEX)[1].trim();
+        var $task_location = $task_html.match(REGEX_TASK)[1].trim();
         if ($task_location.length) {
-            var temperature;
             var url_get = BASE_URL + $task_location;
             $.ajax({
-                async: false,
                 type: 'GET',
                 url: url_get,
+                context: {'task': $current_task},
                 success: function(data) {
-                    temperature = data.temperature;
-                }
+                    var temperature = data.temperature;
+                    var colour = get_colour(temperature);
+                    this.task.css('background-color', colour);                }
             });
-            var colour = get_colour(temperature); 
-            $current_task.css('background-color', colour);
         }
     }
 
+    // Show weather at the beginning for an update.
+    var $update_marker = $(".temperature");
+    if ($update_marker.length) {
+        var $location = $update_marker.prop('outerHTML');
+        if (REGEX_LOCATION.test($location)) {
+            var start_location = $update_marker.prop('outerHTML').match(REGEX_LOCATION)[1];
+            var url_get = BASE_URL + start_location;
+            $.ajax({
+                type: 'GET',
+                url: url_get,
+                context: {
+                    'marker': $update_marker,
+                    'form': $('.todo_form')
+                },
+                success: function(data) {
+                    var temperature = data.temperature;
+                    var colour = get_colour(temperature);
+                    this.form.css('background-color', colour);
+                    this.marker.html(`<strong>Current Temperature: ${temperature}C</strong>`);
+                }
+            });
+        }
+    }
+    
     // Let location decide the colour of 'Create' and 'Update' forms.
     var $location = $('#id_location');
     if ($location.length) {
-        var csrfToken = $('input[name=csrfmiddlewaretoken]').val();
         $location.on('input', function(){
             var location = $location.val();
             var $form = $('.todo_form');
             if ($form.length) {
                 var url_get = BASE_URL + location;
                 $.ajax({
-                    async: false,
                     type: 'GET',
                     url: url_get,
+                    context: {'form': $form},
                     success: function(data) {
                         var temperature = data.temperature;
                         var colour = get_colour(temperature);
-                        $form.css('background-color', colour);
+                        this.form.css('background-color', colour);
                         var $temperature_text = $('.temperature');
                         if ($temperature_text.length) {
                             $temperature_text.html(`<strong>Current Temperature: ${temperature}C</strong>`);
